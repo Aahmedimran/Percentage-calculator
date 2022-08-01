5b1b381b2c4881af7dcc993173d791ded793be56
ahmed imran <ahmed.eng2709@gmail.com>
2022-08-01 17:02:09 +0500

    create arrow function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function cal() {
 
+const cal = () =>{
 
     let totalmarks = document.querySelector("#totalmarks").value;
     let obtaniendmarks = document.querySelector("#obtaniendmarks").value;
     let result = obtaniendmarks * 100 / totalmarks;
-    document.querySelector("#result").innerHTML = `you have ${result}%`;
+    document.querySelector("#result").innerHTML = `Your Percentage Is ${result}%`;
 }
